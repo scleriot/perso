66d00f92ebc7a46964b717339555e7df1a77857d
Simon CLERIOT <simon@cleriot.com>
2015-07-21 18:15:37 +0900

    Round profile picture

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require("header.php");
             <div class="col-xs-3 visible-xs"></div>
             <div class="col-xs-6 col-md-2 project img-gray">
                 <a href="projects/qqdroid/" class="ajax-popup">
-                    <img src="projects/qqdroid/miniature.png" class="img-circle img-responsive" />
+                    <img src="projects/qqdroid/miniature.png" class="img-responsive" />
                     <div class="details">
                         <h5>QQDroid</h5>
                     </div>
@@ -18,7 +18,7 @@ require("header.php");
             <div class="col-xs-3 visible-xs"></div>
             <div class="col-xs-6 col-md-2 project img-gray">
                 <a href="projects/yaam/">
-                    <img src="projects/yaam/miniature.png" class="img-circle img-responsive" />
+                    <img src="projects/yaam/miniature.png" class="img-responsive" />
                     <div class="details">
                         <h5>YAAM</h5>
                     </div>
@@ -27,7 +27,7 @@ require("header.php");
             <div class="col-xs-3 visible-xs"></div>
             <div class="col-xs-6 col-md-2 project img-gray">
                 <a href="projects/flirtymob/">
-                    <img src="projects/flirtymob/miniature.png" class="img-circle img-responsive" />
+                    <img src="projects/flirtymob/miniature.png" class="img-responsive" />
                     <div class="details">
                         <h5>Flirtymob</h5>
                     </div>
@@ -44,7 +44,7 @@ require("header.php");
             <div class="col-xs-3 visible-xs"></div>
             <div class="col-xs-6 col-md-2 project img-gray">
                 <a href="projects/mrgm/">
-                    <img src="projects/mrgm/miniature.png" class="img-circle img-responsive" />
+                    <img src="projects/mrgm/miniature.png" class="img-responsive" />
                     <div class="details">
                         <h5>MRGM</h5>
                     </div>
@@ -54,7 +54,7 @@ require("header.php");
             <div class="col-xs-3 visible-xs"></div>
             <div class="col-xs-6 col-md-2 project img-gray">
                 <a href="projects/erpaei/">
-                    <img src="projects/erpaei/miniature.png" class="img-circle img-responsive" />
+                    <img src="projects/erpaei/miniature.png" class="img-responsive" />
                     <div class="details">
                         <h5>ERP</h5>
                     </div>
@@ -64,7 +64,7 @@ require("header.php");
             <div class="col-xs-3 visible-xs"></div>
             <div class="col-xs-6 col-md-2 project img-gray">
                 <a href="projects/borderline/">
-                    <img src="projects/borderline/miniature.png" class="img-circle img-responsive" />
+                    <img src="projects/borderline/miniature.png" class="img-responsive" />
                     <div class="details">
                         <h5>Bord'erline</h5>
                     </div>
@@ -74,7 +74,7 @@ require("header.php");
             <div class="col-xs-3 visible-xs"></div>
             <div class="col-xs-6 col-md-2 project img-gray">
                 <a href="projects/frandroid/">
-                    <img src="projects/frandroid/miniature.png" class="img-circle img-responsive" />
+                    <img src="projects/frandroid/miniature.png" class="img-responsive" />
                     <div class="details">
                         <h5>Comparateur<br />FrAndroid</h5>
                     </div>
